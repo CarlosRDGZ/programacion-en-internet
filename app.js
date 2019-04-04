9441fec44f377611eada94840e8324bbb5546796
Carlos Francisco Rodríguez Aguilar <crodriguez20@ucol.mx>
2019-04-04 07:55:07 -0600

    4 de abril. Templates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var fs = require('fs')
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    var html = fs.readFileSync(`${__dirname}/index.htm`);
-    res.end(html);
+    var html = fs.readFileSync(`${__dirname}/index.htm`, 'utf8');
+    var message = 'Hello World!...';
+    res.end(html.replace('{Message}', message));
 }).listen(1337, '127.0.0.1');
